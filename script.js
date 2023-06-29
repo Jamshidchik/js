55c6ffcb82582081ed53350d0e6e951dc921a569
Jamshid <murodovjamshid347@gmail.com>
2023-06-29 18:50:06 +0500

    js_finished

---
+++ script.js
@@ -0,0 +1,60 @@
+var modal = prompt( "Ismingizni kiriting")
+console.log("Sizning ismingiz " + modal);
+
+var modal = prompt( "Yoshingizni kiriting ")
+console.log("Sizning yoshingiz "+ modal);
+
+var modal = prompt( "6+4")
+if (modal == (6+4)) {
+  alert( "Sizning javobiniz to'gri")
+  console.log("Sizning javobiniz to'gri");
+
+}
+else{
+  alert( "Sizning javobingiz xato; "+ "To'g'ri javob: 10"  )
+  console.log("To'g'ri javob: 6+4= "+ (6+4 ) + " Sizning javobingiz:" + modal);
+}
+
+var modal = prompt( "20-5")
+if ( modal== (20-5)) {
+  alert("Sizning javobiniz to'gri")
+  console.log("Sizning javobiniz to'gri");
+}
+else{
+  alert("Sizning javobingiz xato; "+ "To'g'ri javob: 15" )
+ console.log("To'g'ri javob: 20-5= "+ (20-5)+ " Sizning javobingiz"+ modal)
+}
+
+var modal = prompt( "16*5")
+if(modal== (16*5)) {
+  alert("Sizning javobingiz to'g'ri")
+  console.log("Sizning javobingiz to'g'ri");
+}
+else{
+  alert("Sizning javobingiz xato; "+ "To'g'ri javob "+ (16*5))
+  console.log("To'g'ri javob: 16*5= "+ (16*5 ) + " Sizning javobingiz:" + modal);
+}
+
+var modal = prompt( "12/4")
+if(modal== (12/4))  {
+  alert( "Sizning javobingiz to'g'ri")
+  console.log("Sizning javobingiz to'g'ri");
+}
+else{
+  alert("Sizning javobingiz xato; "+ "To'g'ri javob "+ (12/4))
+  console.log(" To'g'ri javob: 12/4= "+ (12/4 ) + " Sizning javobingiz:" + modal);
+}
+
+var modal = prompt( "6/5")
+if( modal== (6/5)) {
+  alert( "Tashakkur sizning javobingiz to'g'ri")
+  console.log("Tashakkur sizning javobingiz to'g'ri");
+}
+else {
+  alert("Sizning javobingiz xato; "+ "To'g'ri javob: "+ (6/5))
+  console.log("To'g'ri javob: 6/5= "+ (6/5 ) + " Sizning javobingiz:" + modal);
+}
+
+
+
+
